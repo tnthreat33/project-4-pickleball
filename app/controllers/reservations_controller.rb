d7f6d2c04fdd1376f243e39e24bb075b73d61ea5
Rachel Hamby <tnthreat33@gmail.com>
2023-06-18 13:03:38 -0400

    create new reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,22 @@
 class ReservationsController < ApplicationController
-
-    def create 
-        reservation = Reservation.create(reservation_params)
+    def create
+      reservation = Reservation.new(reservation_params)
+  
+      if reservation.save
         render json: reservation, status: :created
-    end 
-private
-    def reservation_params
-        params.permit(:courtId, :userId, :date, :start_time, :end_time)
+      else
+        render json: { error: 'Error creating reservation' }, status: :unprocessable_entity
+      end
     end
-end 
+  
+    private
+  
+    def reservation_params
+        params.require(:reservation).permit(:date, :startTime, :endTime).merge(
+          court_id: params[:court]
+        )
+      end
+      
+      
+  end
+  
