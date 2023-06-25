2b304e8e4e6702adb4f607c67c6f08ae2763aef8
Rachel Hamby <tnthreat33@gmail.com>
2023-06-25 10:07:52 -0400

    add styling to user profile

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,29 +1,45 @@
 import React from "react";
+import "./UserProfile.css";
 
-function UserProfile({ user }) {
-  console.log(user)
+function UserProfile({ user, courts }) {
+  
+
+  const getCourtName = (courtId) => {
+    const court = courts.find((court) => court.id === courtId);
+    return court ? court.name : "Unknown Court";
+  };
 
   return (
-    <>
-      <h1>Profile Page</h1>
-      <h3>Name: {user.name}</h3>
-      <h3>Email: {user.email}</h3>
+    <div className="profile-container">
+      <h1> {user.name} Profile Page</h1>
+      <h3>{user.email}</h3>
 
       <h2>Your Reservations:</h2>
-        {user.reservations.length > 0 ? (
-          <ul>
-            {user.reservations.map(reservation => (
-              <li key={reservation.id}>
-                <p>Date: {reservation.date}</p>
-                <p>Start Time: {new Date(reservation.start_time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - 
-    End Time: {new Date(reservation.end_time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No reservations found.</p>
-        )}
-    </>
+      {user.reservations.length > 0 ? (
+        <div className="reservation-container">
+          {user.reservations.map((reservation) => (
+            <div className="reservation-card" key={reservation.id}>
+              <p>Date: {reservation.date}</p>
+              <p>Court: {getCourtName(reservation.court_id)}</p>
+              <p>
+                Start Time:{" "}
+                {new Date(reservation.start_time).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}{" "}
+                - End Time:{" "}
+                {new Date(reservation.end_time).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No reservations found.</p>
+      )}
+    </div>
   );
 }
 
