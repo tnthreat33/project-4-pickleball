a08a5d40addcde5e00b96dfdc3bfdb0e7c82f6ad
Rachel Hamby <tnthreat33@gmail.com>
2023-06-29 20:48:27 -0400

    add collection method for reservation update and delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
   before_action :authorized
+
+  private 
+
+  def authorized 
+    @current_user = User.find_by(id: session[:user_id])
   
-  def authorized
-    return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
   end
-  
-  
+
 
 end
+
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,9 @@
 class ReservationsController < ApplicationController
+  before_action :find_reservation, only: [:destroy, :update]
+  
   def create
     reservation = Reservation.new(reservation_params)
-    reservation.user_id = session[:user_id] 
+    reservation.user_id = @current_user.id
 
     if reservation.save
       render json: reservation, status: :created
@@ -11,22 +13,28 @@ class ReservationsController < ApplicationController
   end
 
   def destroy 
-    reservation = Reservation.find_by(id: params[:id])
-    if reservation
-      reservation.destroy 
-      head :no_content
+    if @reservation
+      if @reservation.user_id == @current_user.id
+        @reservation.destroy 
+        head :no_content
+      else 
+        render json: { error: "Unauthorized: Cannot delete reservation" }, status: :unauthorized
+      end 
     else 
       render json: { error: "Reservation not found" }, status: :not_found
     end 
   end
 
   def update
-    reservation = Reservation.find_by(id: params[:id])
-    if reservation
-      reservation.update(reservation_params)
-        render json: reservation, status: :accepted
+    if @reservation
+      if @reservation.user_id == @current_user.id
+        @reservation.update(reservation_params)
+        render json: @reservation, status: :accepted
+      else
+        render json: { error: "Unauthorized: Cannot update reservation" }, status: :unauthorized
+      end
     else
-      render json: { error: 'Reservation not found' }, status: :not_found
+      render json: { error: "Reservation not found" }, status: :not_found
     end
   end
 
@@ -34,5 +42,9 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.permit(:court_id, :date, :start_time, :end_time)
+  end
+
+  def find_reservation
+    @reservation = Reservation.find_by(id: params[:id])
   end
 end
